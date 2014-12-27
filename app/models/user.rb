d75cd6e428fb04b74704ba5f025381d874e66e0e
Devon Zuegel <devonzuegel@gmail.com>
2014-12-26 21:09:38 -0800

    basic User model is complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
-  validates(:name, presence: true)
+	# Downcase the email attribute before saving the user
+  before_save { self.email = self.email.downcase }
+
+	# Name validations
+  validates :name, presence: true
+  validates :name, length: { maximum: 50 }
+
+	# Email validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true
+  validates :email, length: { maximum: 255 }
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
 end
