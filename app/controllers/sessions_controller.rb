2864ad6c14ecfdfaf7b928f18f94d2879af0220a
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-12-28 15:08:05 -0700

    "Remember me" default enabled; working on checkbox for opt-out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,12 @@ class SessionsController < ApplicationController
   def new
   end
 
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     	log_in(user)
+    	remember(user)
     	redirect_to(user_url(user))
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
@@ -14,7 +16,10 @@ class SessionsController < ApplicationController
     end
   end
 
+
   def destroy
+    log_out() if logged_in?
+    redirect_to(root_url)
   end
 
 end
