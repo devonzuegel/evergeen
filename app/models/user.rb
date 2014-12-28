6ed0d68eeb1ec1722ded69394a616dd68ee9ff44
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-12-28 12:12:44 -0700

    valid login works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
+
 	# Downcase the email attribute before saving the user
   before_save { self.email = self.email.downcase }
+
 
 	# Name validations
   validates :name, presence: true
   validates :name, length: { maximum: 50 }
+
 
 	# Email validations
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,7 +16,18 @@ class User < ActiveRecord::Base
   validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :email, uniqueness: { case_sensitive: false }
 
+
+  # Password validations
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 
 end
