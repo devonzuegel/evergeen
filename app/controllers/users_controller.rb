8501e01f9772fbdcda0caec524ad84f474c45e9b
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-12-29 18:30:38 -0700

    CRUD and authentication completed for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
-  # Only authorized users may access \index, \edit, and \update
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  # Only authorized users may access \index, \edit, \update, & \destroy.
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
-  # Redirects users trying to edit another user’s profile
+  # Restricts access to the 'destroy' action to admins.
+  before_action :admin_user,     only: :destroy
+
+  # Redirects users trying to edit another user’s profile.
   before_action :correct_user,   only: [:edit, :update]
 
 
@@ -53,8 +56,18 @@ class UsersController < ApplicationController
   end
 
 
-  private
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
+
+  private  # ------------------------------------------------------------
+
+    # NOTE: In particular, 'admin' is not in the list of permitted
+    # attributes. This is what prevents arbitrary users from granting
+    # themselves administrative access to our application.
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
@@ -79,4 +92,10 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+    
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,14 +9,14 @@ class UsersControllerTest < ActionController::TestCase
 
 
   test "should redirect index when not logged in" do
-    get :index
-    assert_redirected_to login_url
+    get(:index)
+    assert_redirected_to(login_url)
   end
-  
+
 
   test "should get new" do
-    get :new
-    assert_response :success
+    get(:new)
+    assert_response(:success)
   end
 
 
@@ -36,17 +36,37 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
-    get :edit, id: @user
-    assert flash.empty?
-    assert_redirected_to root_url
+    get(:edit, id: @user)
+    assert(flash.empty?)
+    assert_redirected_to(root_url)
   end
 
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert flash.empty?
-    assert_redirected_to root_url
+    assert(flash.empty?)
+    assert_redirected_to(root_url)
+  end
+
+  test "should redirect destroy when not logged in" do
+    # Make sure that the user count doesn’t change 
+    assert_no_difference 'User.count' do
+      delete(:destroy, id: @user)
+    end
+
+    assert_redirected_to(login_url)
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+
+    # Make sure that the user count doesn’t change 
+    assert_no_difference 'User.count' do
+      delete(:destroy, id: @user)
+    end
+    
+    assert_redirected_to(root_url)
   end
 
 end
