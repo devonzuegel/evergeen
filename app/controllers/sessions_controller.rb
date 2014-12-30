e3b518ee9f1944d219d09e016ef246f5ce354f81
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-12-30 13:51:38 -0700

    the basic functionality of user activation is done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-    	log_in(user)
-
-    	# Handling the submission of the “remember me” checkbox.
-      (params[:session][:remember_me] == '1') ? remember(user) : forget(user)
-
-      # Redirects to stored location (or to the user's page)
-      redirect_back_or(user)
+      if user.activated?
+        log_in(user)
+        # Handling the submission of the “remember me” checkbox.
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        # Redirects to stored location (or to the user's page)
+        redirect_back_or(user)
+      else
+        flash[:warning] = "Account not activated. Check your email for the activation link."
+        redirect_to(root_url)
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render('new')
