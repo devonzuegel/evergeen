c1e673c51e4d69d0e0163b3999e51c74b88d13a3
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-12-31 23:53:41 -0800

    Added user_id attribute (including validation + tests) on Micropost model

--- db/migrate/20150101073631_create_microposts.rb
+++ db/migrate/20150101073631_create_microposts.rb
@@ -6,6 +6,13 @@ class CreateMicroposts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_foreign_key :microposts, :users
+
+    ##
+    # We expect to retrieve all the microposts associated with a given
+    # user id in reverse order of creation.
+    ##
+    # By including both the user_id and created_at columns as an array,
+    # we arrange for Rails to create a multiple key index.
+    add_index :microposts, [:user_id, :created_at]
   end
 end
