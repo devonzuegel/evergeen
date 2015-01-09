898fd89b6b9f9db85bfee11c3d0b659a72bd8876
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-01-09 12:52:50 -0800

    Text micropost functionality complete (including tests); now need to add multimedia (starting with images)

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
 
+
   def setup
     @micropost = microposts(:orange)
   end
+
 
   test "should redirect create when not logged in" do
     assert_no_difference 'Micropost.count' do
@@ -13,11 +15,26 @@ class MicropostsControllerTest < ActionController::TestCase
     assert_redirected_to(login_url)
   end
 
+
   test "should redirect destroy when not logged in" do
     assert_no_difference 'Micropost.count' do
       delete(:destroy, id: @micropost)
     end
     assert_redirected_to(login_url)
   end
+
+  
+  ##
+  # Make sure one user can’t delete the microposts of a different user,
+  # and we also check for the proper redirect.
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete(:destroy, id: micropost)
+    end
+    assert_redirected_to(root_url)
+  end
+
 
 end
---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+
+  ##
+  # Log in, check the micropost pagination, make an invalid submission,
+  # make a valid submission, delete a post, and then visit a second
+  # user’s page to make sure there are no “delete” links. 
+  test "micropost interface" do
+    log_in_as(@user)
+    get(root_path)
+    assert_select('div.pagination')
+
+    # Invalid submission
+    assert_no_difference 'Micropost.count' do
+      post(microposts_path, micropost: { content: "" })
+    end
+    assert_select('div#error_explanation')
+
+    # Valid submission
+    content = "This micropost really ties the room together"
+    assert_difference 'Micropost.count', 1 do
+      post(microposts_path, micropost: { content: content })
+    end
+    assert_redirected_to(root_url)
+
+    ##
+    # Uses post followed by follow_redirect! in place of the equivalent
+    # post_via_redirect in anticipation of the image upload test.
+    follow_redirect!
+    assert_match(content, response.body)
+
+    # Delete a post.
+    assert_select('a', text: 'delete')
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+
+    # Visit a different user.
+    get(user_path(users(:archer)))
+    # Ensure that there aren't any available delete links
+    assert_select('a', text: 'delete', count: 0)
+  end
+
+end
