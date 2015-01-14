bf56743097920445b66b5b708a1de27a6c1aedef
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-01-13 23:58:37 -0800

    Getting started on ch12

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -6,7 +6,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "password resets" do
+  test "password resets" do 
     get(new_password_reset_path)
     assert_template('password_resets/new')
 
