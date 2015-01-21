803427f3969b31b68d6b31f3bdb245869e389eec
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-01-21 13:48:20 -0800

    Working through ch12; about to run erb2slim

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,13 +9,25 @@ class RelationshipsController < ApplicationController
 
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to(user)
+    current_user.follow(@user)
+    respond_to do |format|
+      ##
+      # Only one of the following lines gets executed. (In this sense,
+      # respond_to is like an if-then-else statement.) This causes the
+      # actions to "degrade gracefully", which means that they work fine
+      # in browsers that have JavaScript disabled.
+      format.html { redirect_to(@user) }
+      format.js
+    end
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to(user)
+    current_user.unfollow(@user)
+    respond_to do |format|
+      # Same "degrade gracefully" behavior as in 'create' fn.
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
