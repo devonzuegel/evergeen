5243d0ecf65cea3477652540514c88533f8e4cd4
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-01-21 21:16:38 -0800

    Fixed password reset errors that broke integration tests

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -27,4 +28,44 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
   
+
+
+  ### FOLLOWING USERS ###
+
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post(relationships_path, followed_id: @other.id)
+    end
+  end
+
+  ##
+  # Testing the Ajax version is almost the same, with 'xhr :post'
+  # in place of plain post.
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      xhr(:post, relationships_path, followed_id: @other.id)
+    end
+  end
+
+
+
+  ### UNFOLLOWING USERS ###
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete(relationship_path(relationship))
+    end
+  end
+
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      xhr(:delete, relationship_path(relationship))
+    end
+  end
+
 end
