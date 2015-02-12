5aa395e8e35b4ce5a839f844bdd15d854dc98b06
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-02-12 00:55:02 -0800

    Reorganized messy custom.scss file; now using .sass instead of .scss for all styles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+    at_home = true
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
