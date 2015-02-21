400aeeb5d8faf691067fb5e5343d4e990c954bcf
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-02-21 04:29:06 -0800

    Set up base Transaction tests

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = users(:michael)
+		@transaction = Transaction.new(
+			amount: 42,
+			stripe_id: nil,
+			description: 'Test transaction',
+			charged: false,
+			user_id: @user.id
+		)
+	end
+
+
+	test 'should be valid' do
+		assert @transaction.valid?
+	end
+
+
+	# Tests the validation that the user_id is present.
+	test 'user id should be present' do
+		@transaction.user_id = nil
+		assert_not @transaction.valid?
+	end
+
 end
